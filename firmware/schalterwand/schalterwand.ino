0ebf58d727887262f296cf8f158ddb1b5db39deb
coon <coon@mailbox.org>
2018-12-09 16:39:25 +0100

    firmware: set pin modes

--- firmware/schalterwand/schalterwand.ino
+++ firmware/schalterwand/schalterwand.ino
@@ -1,5 +1,19 @@
+#define BL1 2
+#define BL2 5
+#define BL3 3
+#define BL4 4
+#define SER_IN 13
+#define SRCLK 6
+#define RCLK A2
+
 void setup() {
-  
+  pinMode(BL1, INPUT);
+  pinMode(BL2, INPUT);
+  pinMode(BL3, INPUT);
+  pinMode(BL4, INPUT);
+  pinMode(SER_IN, OUTPUT);
+  pinMode(RCLK, OUTPUT);
+  pinMode(SRCLK, OUTPUT);
 }
 
 void loop() {
