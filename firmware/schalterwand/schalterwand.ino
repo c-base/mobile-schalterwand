3631b8336dac0c241cb44e01f22a6804019c7b49
coon <coon@mailbox.org>
2018-12-09 17:44:49 +0100

    firmware: use hardware SPI pins, implement basic firmware

--- firmware/schalterwand/schalterwand.ino
+++ firmware/schalterwand/schalterwand.ino
@@ -6,16 +6,75 @@
 #define SCK 6
 #define LATCH A2
 
+#include <SPI.h>
+
+struct R1 {
+  uint8_t
+          : 1,
+  Br2 : 1,
+  Y1   : 1;
+};
+
+struct R2 {
+  uint8_t
+  LED_A : 1,
+  LED_B : 1,
+  LED_C : 1,
+  LED_D : 1,
+  Y2        : 1,
+  Y3        : 1,
+  Br3      : 1,
+  Br1      : 1;
+};
+
+struct R3 {
+  uint8_t
+  LED_E : 1,
+  LED_F : 1,
+  LED_G: 1,
+  LED_H: 1;
+};
+
 void setup() {
   pinMode(BL1, INPUT);
   pinMode(BL2, INPUT);
   pinMode(BL3, INPUT);
   pinMode(BL4, INPUT);
-  pinMode(MOSI, OUTPUT);
-  pinMode(LATCH, OUTPUT);
-  pinMode(SCK, OUTPUT);
+  pinMode(MOSI, OUTPUT); // 11
+  pinMode(LATCH, OUTPUT); // A2
+  pinMode(SCK, OUTPUT); // 13
+
+  Serial.begin(115200);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV128);
 }
 
 void loop() {
+  R1 r1 = {0};
+  R2 r2 = {0};
+  R3 r3 = {0};
+
+  r2.LED_A = 1;
+  r2.LED_B = 0;
+  r2.LED_C = 0;
+  r2.LED_D = 0;
+  r3.LED_E = 0;
+  r3.LED_F = 0;
+  r3.LED_G = 0;
+  r3.LED_H = 0;
+
+  digitalWrite(LATCH, LOW);
+  delay(10);
+
+  SPI.transfer(*reinterpret_cast<uint8_t*>(&r1));
+  delay(10);
+  SPI.transfer(*reinterpret_cast<uint8_t*>(&r2));
+  delay(10);
+  SPI.transfer(*reinterpret_cast<uint8_t*>(&r3));
+
+  delay(10);
+  digitalWrite(LATCH, LOW);
   
+  while(true)
+    ;
 }
