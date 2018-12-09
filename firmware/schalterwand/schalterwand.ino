2c85f2dfffd4218566f5a20a4451df21b72a45ed
coon <coon@mailbox.org>
2018-12-09 17:09:55 +0100

    firmware: use better pin names

--- firmware/schalterwand/schalterwand.ino
+++ firmware/schalterwand/schalterwand.ino
@@ -2,18 +2,18 @@
 #define BL2 5
 #define BL3 3
 #define BL4 4
-#define SER_IN 13
-#define SRCLK 6
-#define RCLK A2
+#define MOSI 13
+#define SCK 6
+#define LATCH A2
 
 void setup() {
   pinMode(BL1, INPUT);
   pinMode(BL2, INPUT);
   pinMode(BL3, INPUT);
   pinMode(BL4, INPUT);
-  pinMode(SER_IN, OUTPUT);
-  pinMode(RCLK, OUTPUT);
-  pinMode(SRCLK, OUTPUT);
+  pinMode(MOSI, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+  pinMode(SCK, OUTPUT);
 }
 
 void loop() {
